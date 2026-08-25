e6c9c509958dc5f6637109e922f77234499ffc2f
White Red <rednblkx@protonmail.com>
2026-08-25 22:28:41 +0300

    refactor(ddk): remove obsolete reader, issuer, and endpoint structs
    - Remove `readerData_t`, `hkIssuer_t`, and `hkEndpoint_t` superseded by
    the `ddk::store` models
    - Remove unused header includes and commented-out enrollment structures

--- include/DDKReaderData.h
+++ include/DDKReaderData.h
@@ -1,8 +1,5 @@
 #pragma once
 
-#include <cstdint>
-#include <vector>
-
 typedef enum {
   kAliro = 0x00,
   kHomeKey = 0x01
@@ -42,46 +39,3 @@ typedef enum
   kFlowNext = 0xFF,
   kFlowFailed = -1
 } KeyFlow;
-
-// struct hkEnrollment_t
-// {
-//   std::time_t unixTime = 0;
-//   std::vector<uint8_t> payload;
-// };
-// struct hkEnrollments_t
-// {
-//   hkEnrollment_t hap;
-//   hkEnrollment_t attestation;
-// };
-
-struct hkEndpoint_t
-{
-  std::vector<uint8_t> endpoint_id;
-  uint32_t last_used_at = 0;
-  uint8_t counter = 0;
-  uint8_t key_type = 0;
-  std::vector<uint8_t> endpoint_pk;
-  std::vector<uint8_t> endpoint_pk_x;
-  std::vector<uint8_t> endpoint_prst_k;
-  // hkEnrollments_t enrollments;
-};
-
-struct hkIssuer_t
-{
-  std::vector<uint8_t> issuer_id;
-  std::vector<uint8_t> issuer_pk;
-  std::vector<uint8_t> issuer_pk_x;
-  std::vector<hkEndpoint_t> endpoints;
-};
-
-struct readerData_t
-{
-  readerData_t() : reader_sk(0), reader_pk(0), reader_pk_x(0), reader_gid(0), reader_id(0), issuers(0) {}
-  std::vector<uint8_t> reader_sk;
-  std::vector<uint8_t> reader_pk;
-  std::vector<uint8_t> reader_pk_x;
-  std::vector<uint8_t> reader_gid;
-  std::vector<uint8_t> reader_id;
-  std::vector<hkIssuer_t> issuers;
-};
-
